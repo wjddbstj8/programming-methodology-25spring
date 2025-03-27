e654d5f4627d1ccf379bdd3a175b9953e80c09ec
Suhwan <sshkeb96@snu.ac.kr>
2025-03-27 11:57:07 +0900

    init

---
+++ calculator.cpp
@@ -0,0 +1,18 @@
+#include "calculator.h"
+#include <stdexcept>
+
+int Calculator::add(int a, int b) {
+    // TODO
+}
+
+int Calculator::sub(int a, int b) {
+    // TODO
+}
+
+int Calculator::mul(int a, int b) {
+    // TODO
+}
+
+int Calculator::div(int a, int b) {
+    // TODO
+}
